b330b67a3b021d13a339590f9f4d5026640cc667
wedjg <125490772@qq.com>
2019-05-22 13:44:29 +0800

    feat: 增加邮件模板

--- src/test/java/com/wedjg/advice/service/impl/MailServiceImplTest.java
+++ src/test/java/com/wedjg/advice/service/impl/MailServiceImplTest.java
@@ -7,9 +7,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -22,16 +26,37 @@ public class MailServiceImplTest {
 
     private MailDto mail;
 
+    TemplateEngine templateEngine;
+
     @Before
     public void setUp() throws Exception {
         mail = new MailDto();
         mail.setTo("125490772@qq.com");
         mail.setSubject("advise的邮件");
         mail.setContent(LocalDateTime.now().toString());
+        templateEngine = new TemplateEngine();
     }
 
     @Test
     public void sendSimpleMail() {
-        mailService.sendHtmlMail(this.mail);
+        //mailService.sendHtmlMail(this.mail);
     }
+
+
+    @Test
+    public void sendTemplateMail() {
+//        Map<String, Object> model = new HashMap();
+//        model.put("username", "didi");
+//        String text = VelocityEngineUtils.mergeTemplateIntoString(
+//                velocityEngine, "template.vm", "UTF-8", model);
+    }
+
+    @Test
+    public void testThymeleaf() {
+        Context context = new Context();
+        context.setVariable("username", "wedjg");
+        String result = templateEngine.process("emailTemplate", context);
+        System.out.println(result);
+    }
+
 }
