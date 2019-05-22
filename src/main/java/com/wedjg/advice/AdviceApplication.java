87e6675dbf43c3f67f37b30e7125665ce6d15e5f
wedjg <125490772@qq.com>
2019-05-22 22:16:11 +0800

    fix: 修正作者注释

--- src/main/java/com/wedjg/advice/AdviceApplication.java
+++ src/main/java/com/wedjg/advice/AdviceApplication.java
@@ -3,6 +3,9 @@ package com.wedjg.advice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wedjg
+ */
 @SpringBootApplication
 public class AdviceApplication {
 
